5601caf384f97e3fc7a516cdd56bc24b1f977ad9
Drew Bragg <DRBragg@gmail.com>
2020-10-06 13:44:42 -0400

    first extraction

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vue Trix Editor
+
+Our version of a Vuejs component for the Trix editor. We're extracting this from our Virtual Advisory Platform and should be considered a work in progress until version 1.
---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue'
+import VueTrixEditor from '@/vue-trix-editor.vue'
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueTrixEditor,
+  },
+})
+</script>
+
+<template>
+  <div id="app">
+    <vue-trix-editor />
+  </div>
+</template>
